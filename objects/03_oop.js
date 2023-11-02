db56837b3f09604c83dab352a93d1a0539cc44c2
John Paul <john.gabule@digitalawesomeapps.com>
2023-11-02 15:45:53 +0800

    learning oop in js

---
+++ objects/03_oop.js
@@ -0,0 +1,63 @@
+class Person {
+  name = '';
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}`);
+  }
+}
+
+class Professor extends Person {
+  teaches;
+
+  constructor(name, teaches) {
+    super(name);
+    this.teaches = teaches;
+  }
+
+  introduceSelf() {
+    console.log(
+      `My name is ${this.name}, and I will be your ${this.teaches} professor.`,
+    );
+  }
+
+  grade(paper) {
+    const grade = Math.floor(Math.random() * (5 - 1) + 1);
+    console.log(grade);
+  }
+}
+
+class Student extends Person {
+  #year;
+
+  constructor(name, year) {
+    super(name);
+    this.#year = year;
+  }
+
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}, and I'm in year ${this.#year}.`);
+  }
+
+  canStudyArchery() {
+    return this.#year > 1;
+  }
+}
+
+// ** Private methods
+class PrivteClass {
+  somePublicMethod() {
+    this.#somePrivateMethod();
+  }
+
+  #somePrivateMethod() {
+    console.log("You called me?");
+  }
+}
+
+const myPrivteClass = new PrivteClass();
+myPrivteClass.somePublicMethod(); // 'You called me?'
+// myPrivteClass.#somePrivateMethod(); // SyntaxError
